1baa25d045b61cb5f0a783bb92207d809e8c61b1
sssungjin <dkdlwls576@gmail.com>
2024-10-08 17:55:34 +0900

    fix: 프록시 설정 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,16 +4,9 @@ module.exports = (app) => {
   app.use(
     "/webchat",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: "http://localhost:8080/webchat",
       changeOrigin: true,
       ws: true,
-      pathRewrite: {
-        "^/webchat": "/webchat", // 필요한 경우 경로 재작성
-      },
-      onError: (err, req, res) => {
-        console.error("Proxy error:", err);
-        res.status(500).send("Proxy error");
-      },
     })
   );
 };
