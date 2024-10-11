1991b3de21b1fd0a48df6b6376a9e6ec9f0ac096
sssungjin <dkdlwls576@gmail.com>
2024-10-11 12:47:25 +0900

    feat: 테스트용 ko, en 선택 버튼 header에 추가

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,19 +1,41 @@
 import React from "react";
 import { Navbar, Nav, NavItem, Button } from "reactstrap";
+import { useTranslation } from "react-i18next";
 import justiaLogo from "../../styles/images/justia_text.png";
 
 const Header = ({ userName, userEmail, onLogout }) => {
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <Navbar color="light" light expand="md" className="mt-2 px-2">
       <div className="container-fluid d-flex justify-content-between align-items-center">
         <div className="d-flex align-items-center">
-          {/* <h3 className="mb-0 ml-2">Justia - Legal Chatbot</h3> */}
           <img
             src={justiaLogo}
             alt="Justia Logo"
             className="mt-2"
             style={{ width: "200px", height: "auto" }}
           />
+        </div>
+        <div className="d-flex justify-content-center flex-grow-1">
+          <Button
+            color="link"
+            onClick={() => changeLanguage("ko")}
+            className="mx-2"
+          >
+            한국어
+          </Button>
+          <Button
+            color="link"
+            onClick={() => changeLanguage("en")}
+            className="mx-2"
+          >
+            English
+          </Button>
         </div>
         <Nav className="d-flex align-items-center" navbar>
           <NavItem className="mr-3">
