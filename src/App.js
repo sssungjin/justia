d62874bcaa87d568063c9ef3765b82d808fc2115
sssungjin <dkdlwls576@gmail.com>
2024-10-11 13:04:23 +0900

    fix: use state current languate navigator language

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.scss";
 import PageRoutes from "./components/PageRoutes";
-import React from "react";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { i18n } = useTranslation();
+  const [currentLanguage, setCurrentLanguage] = useState(
+    navigator.language.split("-")[0]
+  );
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+    setCurrentLanguage(lng);
+  };
+
   return (
     <div className="App">
-      <PageRoutes />
+      <PageRoutes
+        currentLanguage={currentLanguage}
+        changeLanguage={changeLanguage}
+      />
     </div>
   );
 }
